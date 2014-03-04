d85834bb325cba8419810d42faa7f3f75a91e6aa
Myk Melez <myk@mozilla.org>
2014-03-04 11:21:23 -0800

    add egg timer and frequency properties

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,9 @@
 var Player = Backbone.Model.extend({
   eggs: 0,
+  eggTimer: null,
+  eggFrequency: 1, // per second
+
   nests: [],
-  // Or do birds only need to be referenced by nests?
-  birds: [],
+  birds: [], // or perhaps birds only need to be referenced by nests?
+  badges: [],
 });
