da5a5a4bbd25c5d287ca377e7d4793770003c615
louisstow <louisstow@gmail.com>
2014-03-04 12:42:51 -0800

    * more nest features

--- src/Nest.js
+++ src/Nest.js
@@ -1,37 +1,37 @@
 var Nest = Backbone.Model.extend({
-	name: null,
-	description: null,
-	image: null,
-	capacity: 0,
-	cost: 0,
+  name: null,
+  description: null,
+  image: null,
+  capacity: 0,
+  cost: 0,
 
-	birds: [],
+  birds: [],
 
-	initialize: function (data) {
-		this.name = data.name;
-		this.image = data.image;
-		this.description = data.description;
-		this.capacity = data.capacity;
-		this.cost = data.cost;
-	},
+  initialize: function (data) {
+    this.name = data.name;
+    this.image = data.image;
+    this.description = data.description;
+    this.capacity = data.capacity;
+    this.cost = data.cost;
+  },
 
-	addBird: function (bird) {
-		if (this.birds.length >= this.capacity) {
-			return false;
-		}
+  addBird: function (bird) {
+    if (this.birds.length >= this.capacity) {
+      return false;
+    }
 
-		this.birds.push(bird);
-		return bird;
-	},
+    this.birds.push(bird);
+    return bird;
+  },
 
-	removeBird: function (bird) {
-		for (var i = 0; i < this.birds.length; ++i) {
-			if (this.birds[i] == bird) {
-				this.birds.splice(i, 1);
-				return bird;
-			}
-		}
+  removeBird: function (bird) {
+    for (var i = 0; i < this.birds.length; ++i) {
+      if (this.birds[i] == bird) {
+        this.birds.splice(i, 1);
+        return bird;
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 });
