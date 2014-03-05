fb5e1d8569a195c8e1f55569848d4a9e220e7eed
Martyn Haigh <martyn.haigh@gmail.com>
2014-03-05 15:05:54 -0800

    awards done

---
+++ src/AwardView.js
@@ -0,0 +1,29 @@
+var AwardView = Backbone.View.extend({
+  id: function() { return this.model.get("id") + "AwardView" },
+
+  initialize: function(data) {
+  	this.listenTo(this.model, "change", this.render);
+    this.view = $("#notifications").append(this.render().$el);
+    this.view.css({opacity: 0, bottom: "-75px"}).animate({bottom: "0", opacity: 1}, 500);
+  },
+
+  template: _.template('<div class="award">' + 
+  	'<p class="description"><%- description %></p>' +
+  	'</div>'),
+
+  Awards: {
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  },
+  
+  hide: function() {
+    this.view.animate({bottom: "-75px", opacity: 0}, () => {
+    	this.remove();
+    });
+
+  } 
+
+});
