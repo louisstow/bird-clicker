62a04b5c3cc832ec8d4f51fa7282eeb7d3ff7bce
louisstow <louisstow@gmail.com>
2014-03-06 10:38:47 -0800

    * Better store styles

--- src/NestView.js
+++ src/NestView.js
@@ -57,7 +57,7 @@ var BuyableNestView = NestView.extend({
     "click": "buy",
   },
 
-  template: _.template('<div class="<%= (eggs > model.cost) || model.shown ? "" : "hidden"  %> <%= eggs > model.cost ? "" : "disabled" %>"><img height=64 src="<%- model.image %>" title="<%- model.name %> - <%- model.description %>"> - <span clss="owned"><%- model.numberOwned %> owned</span><span clss="cost"><%- model.cost %> eggs</span></div>'),
+  template: _.template('<div class="nest <%= (eggs > model.cost) || model.shown ? "" : "hidden"  %> <%= eggs > model.cost ? "" : "disabled" %>"><div class="profile"><img height=64 src="<%- model.image %>" title="<%- model.name %> - <%- model.description %>"></div><div class="info"><strong><%- model.name %></strong><p><%- model.description %></p></div><div class="stats"><span class="owned"><%- model.numberOwned %> owned</span><span class="cost"><%- model.cost %> eggs</span></div></div>'),
 
   buy: function() {
     if(this.model.get("cost") < game.player.get("eggs")) {
