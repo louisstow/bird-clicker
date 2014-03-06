46bdcadb21cbe91e791f55efed48172254fd57cb
Martyn Haigh <martyn.haigh@gmail.com>
2014-03-06 11:53:45 -0800

    adding content

--- content/awards.js
+++ content/awards.js
@@ -2,28 +2,35 @@ var awardData = [
   {
     id: "10_eggs",
     name: "Amateur twitcher",
-    description: "You've collected your first 10 eggs - well done",
+    description: "You've collected your first 10 eggs",
     verify: function() {
     	 return game.player.get("eggs") >= 10;
     }
   }, {
     id: "10_nests",
     name: "Settling in",
-    description: "You've collected your first 10 nests - well done",
+    description: "You've collected your first 10 nests",
     verify: function() {
     	 return game.player.get("nestCount") >= 10;
     }
   }, {
     id: "10_birds",
     name: "Feather lover",
-    description: "You've collected your first 10 birds - well done",
+    description: "You've collected your first 10 birds",
     verify: function() {
-    	 return game.player.get("birdCount") >= 10;
+         return game.player.get("birdCount") >= 10;
+    }
+  },{
+    id: "100_birds",
+    name: "Bird Collector",
+    description: "You've collected your 100 birds",
+    verify: function() {
+         return game.player.get("birdCount") >= 10;
     }
   }, {
     id: "100_clicks",
     name: "Happy clicker",
-    description: "You've clicked 100 times - well done",
+    description: "You've clicked 100 times",
     verify: function() {
     	 return game.player.get("manualClicks") >= 100;
     }
@@ -32,21 +39,56 @@ var awardData = [
     name: "Mouse User!",
     description: "You've clicked 10 times",
     verify: function() {
-    	 return game.player.get("manualClicks") >= 10;
+         return game.player.get("manualClicks") >= 10;
+    }
+  },{
+    id: "100_clicks",
+    name: "Dedicated",
+    description: "You've clicked 100 times",
+    verify: function() {
+         return game.player.get("manualClicks") >= 100;
+    }
+  },{
+    id: "1000_clicks",
+    name: "Mouse Abuser",
+    description: "You've clicked 1000 times",
+    verify: function() {
+         return game.player.get("manualClicks") >= 1000;
+    }
+  },{
+    id: "10000_clicks",
+    name: "Manual Labourer",
+    description: "You've clicked 10000 times",
+    verify: function() {
+         return game.player.get("manualClicks") >= 10000;
     }
   }, {
     id: "100_egg_inc",
     name: "Prolific Layer",
-    description: "You're getting 100 eggs on each lay - well done",
+    description: "You're getting 100 eggs on each lay",
     verify: function() {
     	 return game.player.get("eggIncrement") >= 100;
     }
   }, {
     id: "one_minute",
     name: "The beginning...",
-    description: "You've played for a minute - well done",
+    description: "You've played for a minute",
     verify: function() {
-    	 return game.get("totalTime") >= 60;
+         return game.get("totalTime") >= 60;
+    }
+  }, {
+    id: "one_hour",
+    name: "Bird lover",
+    description: "You've played for an hour",
+    verify: function() {
+         return game.get("totalTime") >= 60 * 60;
+    }
+  }{
+    id: "one_day",
+    name: "Energy Waster",
+    description: "You've played for an entire day.  It doesn't count if you just leave your browser open!",
+    verify: function() {
+         return game.get("totalTime") >= 60 * 60 * 24;
     }
   }
 ];
