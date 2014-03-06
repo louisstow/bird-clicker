ba02b4ec0716f507303265390369303ee5700a60
louisstow <louisstow@gmail.com>
2014-03-06 11:33:47 -0800

    * New STYLES

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -6,7 +6,7 @@ var Scoreboard = Backbone.View.extend({
   },
 
   render: function() {
-    $("#eggs").text("Eggs: " + Math.round(this.model.get("eggs")));
+    $("#eggs").text(Math.round(this.model.get("eggs")));
     $("#increment").text("Laying " + Math.round(this.model.get("eggIncrement") * 1000) / 1000 + " eggs per second");
   },
 
