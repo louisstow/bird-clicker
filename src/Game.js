825213a702586aeda1059fd6fc350150b9a72fc1
Martyn Haigh <martyn.haigh@gmail.com>
2014-03-06 18:09:39 -0800

    change number formatter to return 0 instead of 0.0

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,20 @@
 Number.prototype.formatNumber = function(c, d, t){
+  if(this == 0) {
+    return "0";
+  }
   if (this < 1) {
     return this.toFixed(1);
   }
   if(this - Math.floor(this) != 0) {
     c = 1;
   }
-var n = this, 
-    c = isNaN(c = Math.abs(c)) ? 0 : c, 
-    d = d == undefined ? "." : d, 
-    t = t == undefined ? "," : t, 
-    s = n < 0 ? "-" : "", 
-    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
-    j = (j = i.length) > 3 ? j % 3 : 0;
+  var n = this, 
+      c = isNaN(c = Math.abs(c)) ? 0 : c, 
+      d = d == undefined ? "." : d, 
+      t = t == undefined ? "," : t, 
+      s = n < 0 ? "-" : "", 
+      i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+      j = (j = i.length) > 3 ? j % 3 : 0;
    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
  };
 
