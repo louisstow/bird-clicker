02c3b7889186edfc79eaf0262012e49e333eb915
Myk Melez <myk@mozilla.org>
2014-03-07 10:54:32 -0800

    use the right increment

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -8,31 +8,22 @@ var Scoreboard = Backbone.View.extend({
   },
 
   render: function() {
-    var eggs = this.model.get("eggs");
+    var count = this.model.get("eggs");
+    var increment = game.player.calculateEggsPerSecond();
 
-    document.title = eggs.formatNumber() + " eggs - Twitcher";
+    document.title = count.formatNumber() + " eggs - Twitcher";
 
-    $("#increment").text("Laying " + (Math.round(game.player.calculateEggsPerSecond() * 10) / 10).formatNumber() + " eggs per second");
+    $("#increment").text("Laying " + (Math.round(increment * 10) / 10).formatNumber() + " eggs per second");
 
     if (this.interval) {
       clearInterval(this.interval);
     }
 
-    var increment = this.model.get("eggIncrement");
-    var step = 0, lastCount = eggs;
     this.interval = setInterval(() => {
       // The increment is per-second, and we update the egg counter 20 times
       // per second (i.e. every 50ms), so we divide the increment by 20 to get
       // our increment per interval.
-      var count = eggs + (increment / 20) * (++step);
-
-      // Complain about a bug in our algorithm.  This should never happen!
-      if (lastCount > count) {
-        console.warn("last count " + lastCount + " > " + count);
-      }
-
-      lastCount = count;
-
+      count = count + (increment / 20);
       $("#eggs").text(count.formatNumber(1));
     }, 50);
   },
