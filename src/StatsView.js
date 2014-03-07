a2a4033d73b958814530f6c394a3bba6596bfd6c
Martyn Haigh <martyn.haigh@gmail.com>
2014-03-07 10:41:54 -0800

    add upgrade count to stats view

--- src/StatsView.js
+++ src/StatsView.js
@@ -5,10 +5,11 @@ var StatsView = Backbone.View.extend({
     '<dt>Total Clicks</dt><dd><%= model.manualClicks.formatNumber() %></dd>' + 
     '<dt>Eggs per second</dt><dd><%= (Math.round(model.eggIncrement * 10) / 10).formatNumber() %></dd>' + 
     '<dt>Challenges completed</dt><dd><%= model.challengesCompleted.formatNumber() %></dd>' + 
-    '<dt>Time played</dt><dd><%= model.totalTimePlayed.formatNumber() %> seconds</dd>' +  
-    '<dt>Awards</dt><dd><%= model.rewardedAwards.formatNumber() %> / <%= awards %></dd>' +  
+    '<dt>Time played</dt><dd><%= model.totalTimePlayed.formatNumber() %> seconds</dd>' +   
     '<dt>Birds</dt><dd><%= model.birdCount.formatNumber() %></dd>' +  
     '<dt>Nests</dt><dd><%= model.nestCount.formatNumber() %></dd>' + 
+    '<dt>Awards</dt><dd><%= model.rewardedAwards.formatNumber() %> / <%= totalNumberOfAwards %></dd>' + 
+    '<dt>Upgrades</dt><dd><%= model.purchasedUpgrades.formatNumber() %> / <%= totalNumberOfUpgrades %></dd>' + 
     '</dl>'),
 
   initialize: function() {
@@ -16,7 +17,7 @@ var StatsView = Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html(this.template({model:this.model.attributes, awards:game.awards.length}));
+    this.$el.html(this.template({model:this.model.attributes, totalNumberOfAwards:game.awards.length, totalNumberOfUpgrades:game.upgrades.length}));
     return this;
   },
 
