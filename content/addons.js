6e2195190daf6747e92ea3d4b0f20432651d7af1
Martyn Haigh <martyn.haigh@gmail.com>
2014-03-07 08:30:57 -0800

    add addon

--- content/addons.js
+++ content/addons.js
@@ -16,7 +16,18 @@ var addonData = [
       return game.player.addons.findWhere({id:"robin_01"}) != null;
     },    
     process: function() {
-    	game.player.extraEggs += game.birds.at(0).get("numberOwned") * 0.1;
+      game.player.extraEggs += game.birds.at(0).get("numberOwned") * 0.1;
+    }
+  }, 
+  {
+    id: "robin_03",
+    description: "robins get double eggs a second",
+    cost: 20000,
+    canShow: function() {
+      return game.player.addons.findWhere({id:"robin_02"}) != null;
+    },    
+    process: function() {
+      game.player.extraEggs += game.birds.at(0).get("numberOwned") * 2;
     }
   },
 ];
